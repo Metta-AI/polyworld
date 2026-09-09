6566dddd049a9fc80b590f6f7fabb9a48bc0c40c
Richard Higgins <richard@softmax.com>
2026-09-08 19:03:03 -0700

    Improve narrow gallery copy and control contrast, retain live input diagnostics

--- examples/ui_gallery/build_atlas.nim
+++ examples/ui_gallery/build_atlas.nim
@@ -1,10 +1,12 @@
 ## Build the gallery from the pinned Silky example assets, without game art packs.
-import std/[os], silky/atlas
+import std/os, pixie, silky/atlas
 
 let data = if paramCount() > 0: paramStr(1) else: "../silky/examples/basicwindow/data"
 createDir("tmp/ui-gallery")
 let builder = newAtlasBuilder(2048, 4)
 builder.addDir(data & "/", data & "/")
+doAssert builder.addImage("button.disabled.9patch",
+  readImage(data / "../../the7gui/data/button.disabled.9patch.png"))
 builder.addFont(data / "IBMPlexSans-Regular.ttf", "Default", 18)
 builder.addFont(data / "IBMPlexSans-Regular.ttf", "H1", 30)
 builder.write("tmp/ui-gallery/atlas.png")
